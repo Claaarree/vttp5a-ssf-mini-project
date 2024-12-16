a29008f2c1a44a825fc89f03d04892371c54b16d
Claaarree <clare.fh@hotmail.com>
2024-12-16 21:17:24 +0800

    initial commit

---
+++ src/main/java/sg/edu/nus/iss/vttp5a_ssf_mini_project/Url.java
@@ -0,0 +1,6 @@
+package sg.edu.nus.iss.vttp5a_ssf_mini_project;
+
+public class Url {
+    public static final String baseUrl = "https://platform.fatsecret.com/rest";
+    public static final String searchV3 = "/foods/search/v3";
+}
---
+++ src/main/java/sg/edu/nus/iss/vttp5a_ssf_mini_project/restController/TestRestController.java
@@ -0,0 +1,22 @@
+package sg.edu.nus.iss.vttp5a_ssf_mini_project.restController;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import sg.edu.nus.iss.vttp5a_ssf_mini_project.service.TestService;
+
+@RestController
+@RequestMapping("/api/food")
+public class TestRestController {
+    
+    @Autowired
+    TestService testService;
+
+    @GetMapping
+    public ResponseEntity<String> getSearch() {
+        return testService.getSearch();
+    }
+}
