c782106e08c6e97a1b0ad59225840fbd3680f142
Claaarree <clare.fh@hotmail.com>
2024-12-17 17:57:44 +0800

    added manual validation for search, closes #6

--- src/main/java/sg/edu/nus/iss/vttp5a_ssf_mini_project/controller/SearchController.java
+++ src/main/java/sg/edu/nus/iss/vttp5a_ssf_mini_project/controller/SearchController.java
@@ -5,10 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
@@ -31,10 +29,16 @@ public class SearchController {
 
     @PostMapping()
     public ModelAndView showSearchResults(@RequestParam String searchTerm) {
+        ModelAndView mav = new ModelAndView();
+        if (searchTerm.isBlank()){
+            mav.setViewName("search");
+            mav.addObject("blankError", "Search Term cannot be blank!");
+        } else {
+            mav.setViewName("redirect:/search/{searchTerm}/{pageNumber}");
+            mav.addObject("pageNumber", "0");
+            mav.addObject("searchTerm", searchTerm);
+        }
         
-        ModelAndView mav = new ModelAndView("redirect:/search/{searchTerm}/{pageNumber}");
-        mav.addObject("pageNumber", "0");
-        mav.addObject("searchTerm", searchTerm);
 
         return mav;
     }
